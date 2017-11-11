bd39503358382101921af93d51763ce9aa088db0
isisfrz <31372177+isisfrz@users.noreply.github.com>
2017-11-11 13:41:52 -0600

    Add files via upload

---
+++ Practica 6.sql
@@ -0,0 +1,14 @@
+
+Go
+Create View Empleados AS Select Id_Emp, Id_Puesto, Nom_emp,Em_ApPat  From Empleados
+Go
+
+Select * From Empleados
+
+
+Go
+Create View Ejemplares AS Select Id_Libro, Id_Ejem, Id_Ubi  From Ejemplares
+Go
+
+Select * From Ejemplares
+
